64a09be6c7dc1e40e9841e5345af790bd0847e35
SurinderSingh12 <sabysurinder@gmail.com>
2025-08-18 19:34:47 -0700

    Added SignIn and createSessionCookie functions in auth.action.ts. These two function contains the logic to check if the user exists and then get their cookie for authentication.

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,6 +1,34 @@
 'use server'
 
-import {db} from "@/firebase/admin";
+import {db, auth} from "@/firebase/admin";
+import {cookies} from "next/headers";
+
+const ONE_DAY_MS = 24 * 60 * 60 * 1000;
+const SESSION_COOKIE_NAME = 'session';
+
+export async function signIn(params: SignInParams) {
+    const {email, idToken} = params;
+
+    try {
+        const userRecord = await auth.getUserByEmail(email);
+
+        if(!userRecord) {
+            return {
+                success: false,
+                message: 'User does not exist. Create an account session'
+            }
+        }
+        await setSessionCookie(idToken);
+
+    }catch (error) {
+        console.error(error);
+
+        return{
+            success: false,
+            message: 'Faile to log into an account',
+        }
+    }
+}
 
 export async function signUp(params: SignUpParams) {
     const { uid, name, email} = params;
@@ -35,4 +63,27 @@ export async function signUp(params: SignUpParams) {
             message: 'Failed to create a user'
         }
     }
+}
+
+
+export async function setSessionCookie(idToken: string) {
+    // 1) Create a Firebase session cookie (server-side, using Admin SDK)
+    const sessionCookie = await auth.createSessionCookie(idToken, {
+        expiresIn: ONE_DAY_MS,
+    });
+
+
+    const cookieStore = await cookies();
+    cookieStore.set(SESSION_COOKIE_NAME, sessionCookie, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        path: '/',
+        sameSite: 'lax',
+        // Next.js cookie `maxAge` is in seconds:
+        maxAge: ONE_DAY_MS / 1000,
+        // Or use an absolute expiry instead:
+        // expires: new Date(Date.now() + ONE_DAY_MS),
+    });
+
+    return { success: true };
 }
