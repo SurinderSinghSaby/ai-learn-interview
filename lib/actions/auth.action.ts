20b9448b351a8152f91e0d8b38cdbcf8f1ccd6e4
SurinderSingh12 <sabysurinder@gmail.com>
2025-08-25 19:24:13 -0700

    Add your generated interviews on the home page

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -144,3 +144,22 @@ export async function getInterviewByUserID(userId?: string): Promise<Interview[]
         ...(doc.data() as Omit<Interview, "id">),
     }));
 }
+
+export async function getLatestInterviews(
+    params: GetLatestInterviewsParams
+): Promise<Interview[] | null> {
+    const { userId, limit = 20 } = params;
+
+    const interviews = await db
+        .collection("interviews")
+        .orderBy("createdAt", "desc")
+        .where("finalized", "==", true)
+        .where("userId", "!=", userId)
+        .limit(limit)
+        .get();
+
+    return interviews.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+    })) as Interview[];
+}
